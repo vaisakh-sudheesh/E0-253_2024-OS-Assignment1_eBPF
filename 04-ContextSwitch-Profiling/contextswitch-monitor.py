cc61849ba9f53512ac97ca2c45dd369d21136031
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-02-27 05:48:03 +0530

    High ns value bug still there, but temporary fixed

--- 04-ContextSwitch-Profiling/contextswitch-monitor.py
+++ 04-ContextSwitch-Profiling/contextswitch-monitor.py
@@ -18,42 +18,47 @@ struct sched_event_data_t{
     int active_cpu;
 };
 
-BPF_PERCPU_ARRAY(stats_entryts, u64, 1);
+BPF_ARRAY(stats_entryts, u64);
 BPF_PERF_OUTPUT(events);
 
 int kprobe__schedule(struct pt_regs *ctx) {
     u64 init_z = 0;
-    uint32_t zero = 0;
+    uint32_t idx = bpf_get_smp_processor_id();
 
-    u64 *ts = stats_entryts.lookup_or_init(&zero, &init_z);
+    u64 *ts = stats_entryts.lookup_or_init(&idx, &init_z);
     * ts = bpf_ktime_get_ns();
     return 0;
 }
 
 int kretprobe__schedule(struct pt_regs *ctx) {
     u64 init_z = 0;
-    uint32_t zero = 0;
     struct sched_event_data_t evt_data;
+    int cpuidx = bpf_get_smp_processor_id();
 
     u64 exit_ts = bpf_ktime_get_ns();
-    u64 *start_ts = stats_entryts.lookup(&zero);
+    u64 *start_ts = stats_entryts.lookup(&cpuidx);
     if (!start_ts)
         return 0;
+  
+    // BUG: There seems to be abnormally high delay ns values creeping through
+    // ignoring them for now
+    // Ref [1]: https://github.com/iovisor/bcc/pull/4074
+    // Ref [2]: https://github.com/iovisor/bcc/issues/728
+    if (exit_ts < *start_ts) {
+        return 0;
+    }
 
     /* Compose the event data payload and submit*/
-    evt_data.active_cpu = bpf_get_smp_processor_id();
+    evt_data.active_cpu = cpuidx;
     evt_data.delta = exit_ts - *start_ts;
     events.perf_submit(ctx, &evt_data, sizeof(evt_data));
-
-    * start_ts = 0;
-    
     return 0;
 }
 """
 
 sched_ctxswtch_dequeue = collections.deque()
 sched_ctxswtch_evtcnt = 0
-
+skip_processing = False
 
 def print_schedstats():
     global sched_ctxswtch_min, sched_ctxswtch_max, sched_ctxswtch_avg, sched_ctxswtch_evtcnt
@@ -65,18 +70,22 @@ def print_schedstats():
             )
     
 def process_event(cpu, data, size):
-    global sched_ctxswtch_min, sched_ctxswtch_max, sched_ctxswtch_avg, sched_ctxswtch_evtcnt
+    global skip_processing, sched_ctxswtch_evtcnt
     global b
     try:
+        if (skip_processing == True):
+            return
         event = b["events"].event(data)        
+
+        # BUG: There seems to be abnormally high delay ns values creeping through
+        # ignoring them for now
+        # Ref [1]: https://github.com/iovisor/bcc/pull/4074
+        # Ref [2]: https://github.com/iovisor/bcc/issues/728
         if (event.delta > 8589934591):
             print("TASK: VERY HIGH value cpu %d ctx-switch-lat: %d ns\n\n" % (event.active_cpu,event.delta),)
         else:
             sched_ctxswtch_evtcnt += 1
-            sched_ctxswtch_dequeue.append(event.delta)        
-        # print("TASK: cpu %d ctx-switch-lat: %d ns\n\n" % (event.active_cpu,event.delta), end="")
-        # print("")
-
+            sched_ctxswtch_dequeue.append(event.delta)
     except Exception:
         sys.exit(0)
 
@@ -90,6 +99,7 @@ if __name__ == '__main__':
         try:
             b.perf_buffer_poll()
         except KeyboardInterrupt:
-            print_schedstats()
-            exit()
+            skip_processing = True
+            break
+    print_schedstats()
     
